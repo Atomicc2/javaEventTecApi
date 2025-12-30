530ca3764acfdc594c28d38bb3a1c6d2dea1a116
Anderson S. <andersonacpemp@gmail.com>
2025-12-29 22:49:13 -0300

    Add modify que faltavam

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -2,12 +2,14 @@ package com.eventostec.api.controller;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController()
 @RequestMapping("/api/event")
@@ -25,10 +27,17 @@ public class EventController {
             @RequestParam("state") String state,
             @RequestParam("remote") Boolean remote,
             @RequestParam("eventUrl") String eventUrl,
-            @RequestParam(value = "image", required = false)MultipartFile image) {
+            @RequestParam(value = "image", required = false) MultipartFile image) {
         EventRequestDTO eventRequestDTO = new EventRequestDTO(title, description, date, city, state, remote, eventUrl, image);
         Event newEvent = this.eventService.createEvent(eventRequestDTO);
         return ResponseEntity.ok(newEvent);
+    }
+
+    public ResponseEntity<List<EventResponseDTO>> getEvents(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = ")") int size) {
+        List<EventResponseDTO> allEvents = this.eventService.getEvents(page, size);
+        return ResponseEntity.ok(allEvents);
     }
 
 }
